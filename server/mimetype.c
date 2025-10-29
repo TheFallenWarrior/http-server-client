15897b8a658e720fe8127c1a48852dd234d11df5
João Felipe <84517752+TheFallenWarrior@users.noreply.github.com>
2025-10-29 10:33:36 -0300

    Add support for several MIME types

--- server/mimetype.c
+++ server/mimetype.c
@@ -2,21 +2,51 @@
 
 #include "mimetype.h"
 
-// Tipos de mídia comuns para formatos de texto e imagem
+// Tipos de mídia comuns para formatos Web
 const MimeTypeMap mimeTypes[] = {
+	// Web
 	{".htm",  "text/html"},
 	{".html", "text/html"},
 	{".css",  "text/css"},
 	{".js",   "text/javascript"},
 	{".json", "application/json"},
 	{".xml",  "application/xml"},
+
+	// Fonte
+	{".ttf",  "font/ttf"},
+	{".otf",  "font/otf"},
+	{".woff", "font/woff"},
+	{".woff2","font/woff2"},
+
+	// Texto
+	{".pdf",  "aplication/pdf"},
 	{".txt",  "text/plain"},
+	{".cfg",  "text/plain"},
+	{".conf", "text/plain"},
+	{".log",  "text/plain"},
+	{".csv",  "application/csv"},
+	{".php",  "application/x-httpd-php"},
+
+	// Áudio e vídeo
+	{".mp3",  "audio/mpeg"},
+	{".ogg",  "audio/ogg"},
+	{".oga",  "audio/ogg"},
+	{".wav",  "audio/wav"},
+	{".weba", "audio/webm"},
+	{".mp4",  "video/mp4"},
+	{".mpeg", "video/mpeg"},
+	{".ogv",  "video/ogg"},
+	{".avi",  "video/x-msvideo"},
+	{".webm", "video/webm"},
+
+	// Imagem
 	{".png",  "image/png"},
 	{".jpg",  "image/jpeg"},
 	{".jpeg", "image/jpeg"},
 	{".webp", "image/webp"},
 	{".bmp",  "image/bmp"},
 	{".gif",  "image/gif"},
+	{".svg",  "image/svg+xml"},
 	{".ico",  "image/vnd.microsoft.icon"}
 };
 
